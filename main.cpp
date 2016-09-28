978df357bf168e3e72af160f7181d9f833a574ab
mgalactico <mjarrin@outlook.com>
2016-09-27 18:29:00 -0700

    Added accepting and evaluating 4 units.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
-//Read two ints and stop when | is entered. Use While loop
+//
 #include "../std_lib_facilities.h"
 
 int main() {
-    double min, max, temp;
-    cout << "Please enter value: ";
-    cin >> temp;
-    min = max = temp;
+    double min, max, val1, val2;
+    string unit1, unit2;
+    cout << "Please enter value, and specify if in meters (m), centimeters (cm), feet(f), or inches (in): ";
+    cin >> val1 >> unit1;
+
+    //Change input to cms or leave as is if cms entered
+    if (unit1 == "m")
+        val2 = val1 * 100;
+    else if (unit1 == "in")
+        val2 = val1 * 2.54;
+    else if (unit1 == "f")
+        val2 = val1 * 12 * 2.54;
+    else
+        val2 = val1;
+
+    min = max = val2;
     while (cin) {
-        cout << "Please enter value: ";
-        cin >> temp;
-        if (temp < min) {
-            min = temp;
-            cout << min << " is the smallest number so far." << endl;
-        } else  if (temp > max) {
-            max = temp;
-            cout << max << " is the largest number so far." << endl;
+        cout << "Please enter value, and specify if in meters (m), centimeters (cm), feet(f), or inches (in): ";
+        cin >> val1 >> unit2;
+
+        //Change input to cms or leave as is if cms entered
+        if (unit2 == "m")
+            val2 = val1 * 100;
+        else if (unit2 == "in")
+            val2 = val1 * 2.54;
+        else if (unit2 == "f")
+            val2 = val1 * 12 * 2.54;
+        else
+            val2 = val1;
+
+        //Update min and max values
+        if (val2 < min) {
+            min = val2;
+            cout << min << " cms, " << val1 << unit2 <<  " is the smallest number so far." << endl;
+        } else  if (val2 > max) {
+            max = val2;
+            cout << max << " cms, " << val1 << unit2 <<  " is the largest number so far." << endl;
         }
     }
     return 0;
